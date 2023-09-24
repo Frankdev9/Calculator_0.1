8ecb9b6c7c4377c1675a0ba5ea7c0dad3e068833
FrankDev9 <dmfrankjosue@gmail.com>
2023-09-24 08:24:13 -0400

    Clean: code in js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-
-const agregar = (valor) => document.getElementById('pantalla').value += valor
+const agregar =  (valor) => document.getElementById('pantalla').value += valor
 
 const borrar = () => document.getElementById('pantalla').value = ''
 
 function igual (){
-    const valorPantalla = document.getElementById('pantalla').value
-    const resultado = eval(valorPantalla)
-    document.getElementById('pantalla').value = resultado
+    const agregarPantalla = document.getElementById('pantalla').value
+    const  convert = eval(agregarPantalla)
+    document.getElementById('pantalla').value = convert
 }
